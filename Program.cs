f2427fe5bde9113e375a4867d05ee98c6bbad409
MFlEGENDANTON <jffgx@mail.ru>
2024-02-09 17:40:47 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Studying_Git_For_Visual_Studio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
